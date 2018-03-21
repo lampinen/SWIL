05da9d3a7cf08dd800db8b83a9d0c12fffb364fb
Andrew Lampinen <lampinen@stanford.edu>
2018-03-20 21:09:31 -0700

    Model working except for SW

--- MNIST_autoenc.py
+++ MNIST_autoenc.py
@@ -8,7 +8,14 @@ import tensorflow.contrib.slim as slim
 ###### configuration ###########################################################
 
 config = {
-    "num_runs": 10,
+    "num_runs": 1,
+    "batch_size": 20,
+    "base_learning_rate": 0.01,
+    "new_learning_rate": 0.01,
+    "base_training_epochs": 10,
+    "new_training_epochs": 1,
+    "new_batch_num_replay": 10, # how many of batch of new items are replays
+                                # if replay is on
     "layer_sizes": [128, 64, 32, 64, 128]
 }
 
@@ -18,7 +25,7 @@ config = {
 train_data = np.loadtxt("MNIST_data/mnist_train.csv", delimiter = ",")
 test_data = np.loadtxt("MNIST_data/mnist_test.csv", delimiter = ",")
 
-def process_data(raw_dataset):
+def process_data(dataset):
     """Get data split into dict with labels and images"""
     labels = dataset[:, 0]
     images = dataset[:, 1:]/255.
@@ -30,6 +37,15 @@ test_data = process_data(test_data)
 
 ###### Build model func ########################################################
 
+def softmax(x, T=1):
+    """Compute the softmax function at temperature T"""
+    if T != 1:
+        x /= T
+    x -= np.amax(x)
+    x = np.exp(x)
+    x /= np.sum(x)
+    return x
+
 class MNIST_autoenc(object):
     """MNIST autoencoder architecture, with or without replay buffer"""
 
@@ -38,36 +54,128 @@ class MNIST_autoenc(object):
             replay_type: one of ["None", "Random", "SWIL"]
             layer_sizes: list of the hidden layer sizes of the model
         """
+        self.replay_type = replay_type
+        self.base_lr = config["base_learning_rate"]
+        self.new_lr = config["new_learning_rate"]
+
         self.input_ph = tf.placeholder(tf.float32, [None, 784])
         self.lr_ph = tf.placeholder(tf.float32)
 
-        min_layer_size = min(layer_sizes)
+        self.bottleneck_size = min(layer_sizes)
         net = self.input_ph
         for h_size in layer_sizes:
-            net = slim.layers.fully_connected(net, h_size, activation_fn=tf.nn.relu)
-            if h_size == min_layer_size: 
+            net = slim.layers.fully_connected(net, h_size, activation_fn=tf.nn.leaky_relu)
+            if h_size == self.bottleneck_size: 
                 self.bottleneck_rep = net
         self.output = slim.layers.fully_connected(net, 784, activation_fn=tf.nn.sigmoid)
         self.loss = tf.nn.l2_loss(self.output-self.input_ph)
 
+        self.optimizer = tf.train.GradientDescentOptimizer(self.lr_ph)
+        self.train = self.optimizer.minimize(tf.reduce_mean(self.loss))
 
-    def base_train(self, dataset):
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+
+    def base_train(self, dataset, nepochs=100):
         """Train the model on a dataset"""
-        pass
-    
-    def new_data_train(self, new_dataset, old_dataset):
-        """Assuming the model has been trained on old_dataset, add new_dataset,
-        possibly interleaving from old"""
-        pass
+        batch_size = config["batch_size"]
+        for epoch in range(nepochs):
+            order = np.random.permutation(len(dataset["labels"]))
+            for batch_i in xrange(len(dataset["labels"])//batch_size):
+                this_batch_images = dataset["images"][order[batch_i*batch_size:(batch_i+1)*batch_size], :]
+                self.sess.run(self.train, feed_dict={
+                        self.input_ph: this_batch_images,
+                        self.lr_ph: self.base_lr 
+                    })
+         
+    def new_data_train(self, new_dataset, old_dataset=None, nepochs=100):
+        """Assuming the model has been trained on old_dataset, tune on
+        new_dataset, possibly interleaving from old"""
+        batch_size = config["batch_size"]
+        if self.replay_type != "None":
+            old_batch_size = config["new_batch_num_replay"]
+            new_batch_size = batch_size - old_batch_size 
+
+        if self.replay_type == "SWIL":
+            old_dataset_reps = self.get_reps(old_dataset["images"])
+            # standardize
+            old_dataset_reps_means = np.mean(old_dataset_reps, axis=0)
+            old_dataset_reps_sds = np.std(old_dataset_reps, axis=0)
+            old_dataset_reps = (old_dataset_reps - old_dataset_reps_means)/old_dataset_reps_sds
+        
+        for epoch in range(nepochs):
+            order = np.random.permutation(len(new_dataset["labels"]))
+            if self.replay_type == "Random":
+                old_order = np.random.permutation(len(old_dataset["labels"]))
+
+            for batch_i in xrange(len(new_dataset["labels"])//batch_size):
+                if self.replay_type == "None":
+                    this_batch_images = new_dataset["images"][order[batch_i*batch_size:(batch_i+1)*batch_size], :]
+                elif self.replay_type == "Random":
+                    this_batch_images = np.concatenate(
+                        [new_dataset["images"][order[batch_i*new_batch_size:(batch_i+1)*new_batch_size], :],
+                         old_dataset["images"][old_order[batch_i*old_batch_size:(batch_i+1)*old_batch_size], :]])
+                else: # SWIL
+                    this_batch_new = new_dataset["images"][order[batch_i*new_batch_size:(batch_i+1)*new_batch_size], :]
+                    this_batch_new_reps = self.get_reps(this_batch_new)
+                    # standardize
+                    this_batch_new_reps_reps = (this_batch_new_reps - old_dataset_reps_means)/old_dataset_reps_sds
+
+                    dots = np.dot(old_dataset_reps, this_batch_new_reps.transpose())
+
+                    probabilities = softmax(np.amax(dots, axis=1)) 
+                    print(probabilities, np.shape(probabilities))
+                
+                    # note that this is actually pretending that sampling with
+                    # replacement is sampling without, but on a dataset this 
+                    # large that shouldn't really be an issue
+                    replay_samples = np.where(np.random.multinomial(old_batch_size, probabilities)) 
+                    print(replay_samples)
+                    
+                    this_batch_images = np.concatenate(
+                        [this_batch_new,
+                         old_dataset["images"][replay_samples]])
+
+                self.sess.run(self.train, feed_dict={
+                        self.input_ph: this_batch_images,
+                        self.lr_ph: self.new_lr 
+                    })
+
+    def get_reps(self, images):
+        """Gets bottleneck reps for the given images"""
+        batch_size = config["batch_size"]
+        reps = np.zeros([len(images), self.bottleneck_size])
+        for batch_i in xrange((len(images)//batch_size) + 1):
+            this_batch_images = images[batch_i*batch_size:(batch_i+1)*batch_size, :]
+            reps[batch_i*batch_size:(batch_i+1)*batch_size, :] = self.sess.run(
+                self.bottleneck_rep, feed_dict={
+                    self.input_ph: this_batch_images 
+                })
+        return reps
+
+    def get_loss(self, images):
+        """Gets losses for the given images"""
+        batch_size = config["batch_size"]
+        loss = np.zeros([len(images)])
+        for batch_i in xrange((len(images)//batch_size) + 1):
+            this_batch_images = images[batch_i*batch_size:(batch_i+1)*batch_size, :]
+            loss[batch_i*batch_size:(batch_i+1)*batch_size, :] = self.sess.run(
+                self.loss, feed_dict={
+                    self.input_ph: this_batch_images 
+                })
+        return loss
 
     def eval(self, dataset):
-        """Evaluates model on the given dataset"""
+        """Evaluates model on the given dataset. Returns list of losses where
+        losses[i] is the average loss on digit i"""
         pass
+        losses = self.get_loss(dataset["images"])
+        losses_summarized = [np.sum(losses[dataset["labels"] == i])/np.sum(dataset["labels"] == i) for i in range(10)]
 
 ###### Run stuff ###############################################################
 
-for left_out_class in range(10):
-    for replay_type in ["None", "Random", "SWIL"]:
+for left_out_class in range(1):
+    for replay_type in ["SWIL", "Random", "None"]:
         for run in range(config["num_runs"]):
             filename_prefix = "m_%s_lo%i_run%i_" %(replay_type,
                                                    left_out_class,
@@ -77,6 +185,12 @@ for left_out_class in range(10):
 
 
             model = MNIST_autoenc(replay_type=replay_type,
-                                  layer_sizes=config["layer_sizes")
+                                  layer_sizes=config["layer_sizes"])
 
-
+            indices = train_data["labels"] == left_out_class
+            this_base_data = {"labels": train_data["labels"][np.logical_not(indices)],
+                              "images": train_data["images"][np.logical_not(indices)]}
+            this_new_data = {"labels": train_data["labels"][indices],
+                             "images": train_data["images"][indices]}
+            #model.base_train(this_base_data, config["base_training_epochs"])
+            model.new_data_train(this_new_data, this_base_data, config["new_training_epochs"])
