cafd9bd2179c611bfc59f85d8ef9eb2fad9f51da
Andrew Lampinen <lampinen@stanford.edu>
2018-03-20 21:44:47 -0700

    Similarity weighting, logging

--- MNIST_autoenc.py
+++ MNIST_autoenc.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 from __future__ import division 
 
+from collections import Counter
+
 import numpy as np
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
@@ -16,6 +18,8 @@ config = {
     "new_training_epochs": 1,
     "new_batch_num_replay": 10, # how many of batch of new items are replays
                                 # if replay is on
+    "softmax_temp": 0.1, # temperature for SWIL replay softmax
+    "output_path": "./results/",
     "layer_sizes": [128, 64, 32, 64, 128]
 }
 
@@ -45,6 +49,10 @@ def softmax(x, T=1):
     x = np.exp(x)
     x /= np.sum(x)
     return x
+
+def to_unit_rows(x):
+    """Converts row vectors of a matrix to unit vectors"""
+    return x/np.expand_dims(np.sqrt(np.sum(x**2, axis=1)), -1)
 
 class MNIST_autoenc(object):
     """MNIST autoencoder architecture, with or without replay buffer"""
@@ -76,8 +84,13 @@ class MNIST_autoenc(object):
         self.sess = tf.Session()
         self.sess.run(tf.global_variables_initializer())
 
-    def base_train(self, dataset, nepochs=100):
+    def base_train(self, dataset, nepochs=100, log_file_prefix=None, test_dataset=None):
         """Train the model on a dataset"""
+        if log_file_prefix is not None:
+            if test_dataset is not None:
+                with open(config["output_path"] + log_file_prefix + "new_train_losses.csv", "a") as fout:
+                    fout.write("epoch, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i\n" % tuple(range(10)))
+
         batch_size = config["batch_size"]
         for epoch in range(nepochs):
             order = np.random.permutation(len(dataset["labels"]))
@@ -87,26 +100,46 @@ class MNIST_autoenc(object):
                         self.input_ph: this_batch_images,
                         self.lr_ph: self.base_lr 
                     })
+
+            # eval
+            if log_file_prefix is not None:
+                if test_dataset is not None:
+                    with open(config["output_path"] + log_file_prefix + "new_train_losses.csv", "a") as fout:
+                        losses = self.eval(test_dataset)
+                        fout.write(("%i" % epoch) + "%i, %i, %i, %i, %i, %i, %i, %i, %i, %i\n" % tuple(losses))
          
-    def new_data_train(self, new_dataset, old_dataset=None, nepochs=100):
+    def new_data_train(self, new_dataset, old_dataset=None, nepochs=100, test_dataset=None, log_file_prefix=None):
         """Assuming the model has been trained on old_dataset, tune on
         new_dataset, possibly interleaving from old"""
+        softmax_temp = config["softmax_temp"]
         batch_size = config["batch_size"]
         if self.replay_type != "None":
             old_batch_size = config["new_batch_num_replay"]
             new_batch_size = batch_size - old_batch_size 
-
-        if self.replay_type == "SWIL":
-            old_dataset_reps = self.get_reps(old_dataset["images"])
-            # standardize
-            old_dataset_reps_means = np.mean(old_dataset_reps, axis=0)
-            old_dataset_reps_sds = np.std(old_dataset_reps, axis=0)
-            old_dataset_reps = (old_dataset_reps - old_dataset_reps_means)/old_dataset_reps_sds
         
+        if log_file_prefix is not None:
+            if self.replay_type != "None": 
+                with open(config["output_path"] + log_file_prefix + "replay_labels_encountered.csv", "a") as fout:
+                    fout.write("epoch, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i\n" % tuple(range(10)))
+            if test_dataset is not None:
+                with open(config["output_path"] + log_file_prefix + "new_train_losses.csv", "a") as fout:
+                    fout.write("epoch, %i, %i, %i, %i, %i, %i, %i, %i, %i, %i\n" % tuple(range(10)))
+
         for epoch in range(nepochs):
+            if self.replay_type != "None":
+                replay_labels_encountered = Counter()
+
             order = np.random.permutation(len(new_dataset["labels"]))
             if self.replay_type == "Random":
                 old_order = np.random.permutation(len(old_dataset["labels"]))
+            elif self.replay_type == "SWIL":
+                old_dataset_reps = self.get_reps(old_dataset["images"])
+                # standardize
+                old_dataset_reps_means = np.mean(old_dataset_reps, axis=0)
+                old_dataset_reps_sds = np.std(old_dataset_reps, axis=0)
+                old_dataset_reps = (old_dataset_reps - old_dataset_reps_means)/old_dataset_reps_sds
+                # normalize
+                old_dataset_reps = to_unit_rows(old_dataset_reps)
 
             for batch_i in xrange(len(new_dataset["labels"])//batch_size):
                 if self.replay_type == "None":
@@ -115,31 +148,49 @@ class MNIST_autoenc(object):
                     this_batch_images = np.concatenate(
                         [new_dataset["images"][order[batch_i*new_batch_size:(batch_i+1)*new_batch_size], :],
                          old_dataset["images"][old_order[batch_i*old_batch_size:(batch_i+1)*old_batch_size], :]])
+                    replay_labels_encountered.update(old_dataset["labels"][old_order[batch_i*old_batch_size:(batch_i+1)*old_batch_size])
                 else: # SWIL
                     this_batch_new = new_dataset["images"][order[batch_i*new_batch_size:(batch_i+1)*new_batch_size], :]
                     this_batch_new_reps = self.get_reps(this_batch_new)
                     # standardize
                     this_batch_new_reps_reps = (this_batch_new_reps - old_dataset_reps_means)/old_dataset_reps_sds
+                    # normalize
+                    this_batch_new_reps = to_unit_rows(this_batch_new_reps)
 
                     dots = np.dot(old_dataset_reps, this_batch_new_reps.transpose())
 
-                    probabilities = softmax(np.amax(dots, axis=1)) 
-                    print(probabilities, np.shape(probabilities))
+                    probabilities = softmax(np.amax(dots, axis=1), T=softmax_temp) 
                 
                     # note that this is actually pretending that sampling with
                     # replacement is sampling without, but on a dataset this 
-                    # large that shouldn't really be an issue
+                    # large that shouldn't really be an issue unless the temp
+                    # is too small...
                     replay_samples = np.where(np.random.multinomial(old_batch_size, probabilities)) 
-                    print(replay_samples)
                     
                     this_batch_images = np.concatenate(
                         [this_batch_new,
                          old_dataset["images"][replay_samples]])
 
+                    replay_labels_encountered.update(old_dataset["labels"][replay_samples])
+
                 self.sess.run(self.train, feed_dict={
                         self.input_ph: this_batch_images,
                         self.lr_ph: self.new_lr 
                     })
+
+                # eval
+                if log_file_prefix is not None:
+                    if self.replay_type != "None": 
+                        with open(config["output_path"] + log_file_prefix + "replay_labels_encountered.csv", "a") as fout:
+                            counts = [replay_labels_encountered[i] for i in range(10)] 
+                            fout.write(("%i" % epoch) + "%i, %i, %i, %i, %i, %i, %i, %i, %i, %i\n" % tuple(counts))
+                    if test_dataset is not None:
+                        with open(config["output_path"] + log_file_prefix + "new_train_losses.csv", "a") as fout:
+                            losses = self.eval(test_dataset)
+                            fout.write(("%i" % epoch) + "%i, %i, %i, %i, %i, %i, %i, %i, %i, %i\n" % tuple(losses))
+
+
+
 
     def get_reps(self, images):
         """Gets bottleneck reps for the given images"""
@@ -168,9 +219,9 @@ class MNIST_autoenc(object):
     def eval(self, dataset):
         """Evaluates model on the given dataset. Returns list of losses where
         losses[i] is the average loss on digit i"""
-        pass
         losses = self.get_loss(dataset["images"])
         losses_summarized = [np.sum(losses[dataset["labels"] == i])/np.sum(dataset["labels"] == i) for i in range(10)]
+        return losses_summarized
 
 ###### Run stuff ###############################################################
 
@@ -192,5 +243,5 @@ for left_out_class in range(1):
                               "images": train_data["images"][np.logical_not(indices)]}
             this_new_data = {"labels": train_data["labels"][indices],
                              "images": train_data["images"][indices]}
-            #model.base_train(this_base_data, config["base_training_epochs"])
+            model.base_train(this_base_data, config["base_training_epochs"])
             model.new_data_train(this_new_data, this_base_data, config["new_training_epochs"])
